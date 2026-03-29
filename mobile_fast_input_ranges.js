5a1dca3ec0ee6369f6620299144e124ddc84733e
Foued A <anhari.foued@gmail.com>
2026-03-29 05:42:44 +0200

    fix ranges clean reset

---
+++ mobile_fast_input_ranges.js
@@ -0,0 +1,85 @@
+const MFI_RANGES_RAW = {
+
+  "40bb": {
+
+    "UTG": {
+      open: [
+        "AA","KK","QQ","JJ","TT","99","88","77",
+        "AKs","AQs","AJs","ATs",
+        "KQs","QJs","JTs",
+        "AKo","AQo","AJo"
+      ],
+      call: [],
+      threebet: []
+    },
+
+    "HJ": {
+      open: [
+        "AA","KK","QQ","JJ","TT","99","88","77","66",
+        "AKs","AQs","AJs","ATs","A9s",
+        "KQs","KJs","KTs",
+        "QJs","QTs",
+        "JTs","T9s",
+        "AKo","AQo","AJo","KQo","KJo"
+      ],
+      call: [],
+      threebet: []
+    },
+
+    "CO": {
+      open: [
+        "AA","KK","QQ","JJ","TT","99","88","77","66","55",
+        "AKs","AQs","AJs","ATs","A9s","A8s","A7s","A6s","A5s",
+        "KQs","KJs","KTs","K9s",
+        "QJs","QTs","Q9s",
+        "JTs","J9s",
+        "T9s","98s",
+        "AKo","AQo","AJo","ATo",
+        "KQo","KJo","KTo","QJo","JTo"
+      ],
+      call: [],
+      threebet: []
+    },
+
+    "BTN": {
+      open: [
+        "AA","KK","QQ","JJ","TT","99","88","77","66","55","44","33","22",
+        "AKs","AQs","AJs","ATs","A9s","A8s","A7s","A6s","A5s","A4s","A3s","A2s",
+        "KQs","KJs","KTs","K9s","K8s","K7s",
+        "QJs","QTs","Q9s","Q8s",
+        "JTs","J9s","J8s",
+        "T9s","T8s",
+        "98s","97s",
+        "87s","86s",
+        "76s","75s",
+        "65s",
+        "AKo","AQo","AJo","ATo","A9o","A8o",
+        "KQo","KJo","KTo",
+        "QJo","QTo",
+        "JTo"
+      ],
+      call: [],
+      threebet: []
+    },
+
+    "SB": {
+      open: [
+        "AA","KK","QQ","JJ","TT","99","88","77","66","55","44","33","22",
+        "AKs","AQs","AJs","ATs","A9s","A8s","A7s","A6s","A5s","A4s","A3s","A2s",
+        "KQs","KJs","KTs","K9s","K8s",
+        "QJs","QTs","Q9s",
+        "JTs","J9s",
+        "T9s",
+        "98s","87s",
+        "AKo","AQo","AJo","ATo","A9o","A8o",
+        "KQo","KJo","KTo",
+        "QJo","QTo",
+        "JTo"
+      ],
+      call: [],
+      threebet: []
+    }
+
+  }
+
+};
